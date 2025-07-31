dcf3e1cb266d96df3b5ca9e76330f9b483f9c2e2
sarath <sarathos584@gmail.com>
2025-07-31 11:42:11 +0530

    feat: add mongodb connection

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import connectDB from './config/db.js'
 
 dotenv.config()  // configure env variables
 
@@ -9,6 +10,9 @@ const port = process.env.PORT || 8000;
 
 
 app.use(express.json()); // Enable parsing of JSON request bodies
+
+connectDB()
+
 app.use(express.urlencoded({ extended: true })); // Enable parsing of URL-encoded request bodies
 app.use(cors({
     origin: '*',
@@ -22,4 +26,15 @@ app.get('/', (req, res) => {
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`); // Log a message when the server starts
+});
+
+// Graceful shutdown
+process.on('SIGINT', async () => {
+  console.log('\nShutting down server...');
+  await mongoose.connection.close();
+  console.log('MongoDB connection closed.');
+  server.close(() => {
+    console.log('Express server closed.');
+    process.exit(0);
+  });
 });
